377d694982b9b2bee0dc668b8a2c460026d77261
Adrian Rodriguez <adrianrp1988@gmail.com>
2026-05-04 11:03:41 -0400

    auth: prevent password resets for LDAP users in PasswordsController

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -12,12 +12,35 @@ class PasswordsController < Devise::PasswordsController
   end
 
   def create
+    # Prevent password reset for LDAP-backed users
+    email = params.dig(resource_name, :email)&.downcase
+    user = User.find_by('lower(email) = ?', email) if email.present?
+
+    if user&.ldap_user?
+      # Do not send reset instructions for LDAP users. Show a non-enumerating message.
+      flash[:alert] = I18n.t('ldap.password_managed', default: 'Password for this account is managed by your organization. Please contact your administrator.')
+      redirect_to new_session_path(resource_name)
+      return
+    end
+
     super do |resource|
       resource.errors.clear unless Docuseal.multitenant?
     end
   end
 
   def update
+    # Prevent applying a password reset for LDAP-backed users
+    token = params.dig(resource_name, :reset_password_token)
+
+    if token.present?
+      resource = resource_class.with_reset_password_token(token)
+      if resource&.ldap_user?
+        flash[:alert] = I18n.t('ldap.password_managed', default: 'Password for this account is managed by your organization. Please contact your administrator.')
+        redirect_to new_session_path(resource_name)
+        return
+      end
+    end
+
     super do |resource|
       Current.user = resource
     end
